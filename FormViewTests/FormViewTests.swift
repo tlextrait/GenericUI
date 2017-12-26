5d4e37679e135c93f0a569673aaf5c198f4b6893
Thomas Lextrait <thomas.lextrait@gmail.com>
2017-12-26 11:07:59 -0800

    added test for forms mixing native and generic inputs

--- FormViewTests/FormViewTests.swift
+++ FormViewTests/FormViewTests.swift
@@ -22,7 +22,7 @@ class FormViewTests: XCTestCase {
     }
     
     /**
-     Tests building a form, filling it and recovering the data
+     Tests building a simple form, filling it and recovering the data
     */
     func testSimpleFormShouldResolveAnOutput() {
         class Person {
@@ -56,6 +56,9 @@ class FormViewTests: XCTestCase {
         XCTAssertEqual(person.firstname, "John")
     }
     
+    /**
+     Tests building a complex form with multiple inputs and spacers, filling it and recovering the data
+     */
     func testComplexFormShouldResolveAnOutput() {
         class Person {
             var firstname = ""
@@ -104,6 +107,67 @@ class FormViewTests: XCTestCase {
         // Test generic outputs
         XCTAssertEqual(firstnameField.output, "John")
         XCTAssertEqual(lastnameField.output, "Smith")
+        XCTAssertEqual(ageField.output, 26)
+        
+        // Ask the form to build the model
+        let person = form.resolve(model: Person())
+        
+        XCTAssertEqual(person.firstname, "John")
+        XCTAssertEqual(person.lastname, "Smith")
+        XCTAssertEqual(person.age, 26)
+    }
+    
+    /**
+     Test building a form that uses a mix of native and generic inputs, filling it and recovering the data
+    */
+    func testFormUsingNativeInputsShouldResolveAnOutput() {
+        class Person {
+            var firstname = ""
+            var lastname = ""
+            var age = 1
+        }
+        
+        let form = UIQuickFormView<Person>()
+        
+        let firstnameField = UIActiveInput<String>()
+        let lastnameField = UITextField(frame: .zero)   // native input
+        let ageField = UIActiveInput<Int>()
+        
+        let firstnameBinding = form.bind(input: firstnameField) { (p: Person, input: UIActiveInput<String>) in
+            guard let firstname = input.output else {
+                return
+            }
+            p.firstname = firstname
+        }
+        let lastnameBinding = form.bind(input: lastnameField) { (p: Person, input: UITextField) in
+            guard let lastname = input.text else {
+                return
+            }
+            p.lastname = lastname
+        }
+        form.addRow([FormElement(firstnameBinding, size: 1), FormElement.spacer(size: 10), FormElement(lastnameBinding, size: 1)])
+        
+        let ageBinding = form.bind(input: ageField) { (p: Person, input: UIActiveInput<Int>) in
+            guard let age = input.output else {
+                return
+            }
+            p.age = age
+        }
+        form.addRow([FormElement(ageBinding, size: 1)])
+        
+        // Pretend typing stuff into the fields
+        firstnameField.output = "John"
+        lastnameField.text = "Smith"
+        ageField.output = 26
+        
+        // Test string outputs
+        XCTAssertEqual(firstnameField.text, "John")
+        XCTAssertEqual(lastnameField.text, "Smith")
+        XCTAssertEqual(ageField.text, "26")
+        
+        // Test generic outputs
+        XCTAssertEqual(firstnameField.output, "John")
+        XCTAssertEqual(lastnameField.text, "Smith")
         XCTAssertEqual(ageField.output, 26)
         
         // Ask the form to build the model
